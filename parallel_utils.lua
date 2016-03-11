486d541aeb8420df552e248eaa4f635f8f87fdb9
taey16 <taey1600@gmail.com>
2016-03-11 09:46:36 +0900

    remove

--- parallel_utils.lua
+++ parallel_utils.lua
@@ -48,37 +48,5 @@ function parallel_utils.makeDataParallel(model, gpus)
 end
 
 
-function parallel_utils.makeDataParallel_cudnn_v3(model, nGPU, primary_gpu_id)
-  if #nGPU > 1 then
-    print('===> Converting module to nn.DataParallelTable')
-    assert(#nGPU <= cutorch.getDeviceCount(), 
-      'number of GPUs less than nGPU specified')
-    local model_single = model
-    -- Split along first (batch) dimension
-    -- in our case it is batch_size
-    model = nn.DataParallelTable(1)
-    for _,gpu_id in pairs(nGPU) do
-      cutorch.setDevice(gpu_id)
-      print('===> DataParallelTable setDevice: '..gpu_id)
-      if gpu_id == primary_gpu_id then
-        model:add(model_single:cuda(), gpu_id)
-      else
-        model:add(model_single:clone():cuda(), gpu_id)
-      end
-    end
-    -- set 'primary' GPU
-    cutorch.setDevice(primary_gpu_id)
-  end
-  return model
-end
-
-
-function parallel_utils.splitDataParallelTable(model)
-  local feature = model:get(1):get(1)
-  local classifier = model:get(2)
-
-  return feature, classifier
-end
-
 return parallel_utils
 
