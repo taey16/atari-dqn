8367fd56e04cb200ec5c56e480686890420dd251
taey16 <taey1600@gmail.com>
2016-03-23 19:42:32 +0900

    remove bn

--- convnet.lua
+++ convnet.lua
@@ -14,7 +14,6 @@ function create_network(args)
     args.hist_len*args.ncols, args.n_units[1],
     args.filter_size[1], args.filter_size[1],
     args.filter_stride[1], args.filter_stride[1],1,1))
-  net:add(cudnn.SpatialBatchNormalization(args.n_units[1], 0.00001, nil, true))
   net:add(cudnn.ReLU(true))
 
   -- Add convolutional layers
@@ -24,15 +23,14 @@ function create_network(args)
       args.n_units[i], args.n_units[i+1],
       args.filter_size[i+1], args.filter_size[i+1],
       args.filter_stride[i+1], args.filter_stride[i+1],0,0))
-    net:add(cudnn.SpatialBatchNormalization(args.n_units[i+1], 0.00001, nil, true))
     net:add(cudnn.ReLU(true))
   end
 
-  local nel = net:cuda():forward(torch.zeros(1,unpack(args.input_dims)):cuda()):nElement()
+  local nel = net:cuda():forward(
+    torch.zeros(1,unpack(args.input_dims)):cuda()):nElement()
 
   -- reshape all feature planes into a vector per example
   net:add(nn.Reshape(nel))
-
   -- fully connected layer
   net:add(nn.Linear(nel, args.n_hid[1]))
   net:add(cudnn.ReLU(true))
@@ -42,13 +40,11 @@ function create_network(args)
     -- add Linear layer
     last_layer_size = args.n_hid[i+1]
     net:add(nn.Linear(args.n_hid[i], last_layer_size))
-    net:add(cudnn.BatchNormalization(last_layer_size, 0.00001, nil, true))
     net:add(cudnn.ReLU(true))
   end
 
   -- add the last fully connected layer (to actions)
   net:add(nn.Linear(last_layer_size, args.n_actions))
-  net:add(cudnn.BatchNormalization(args.n_actions, 0.00001, nil, true))
 
   net:cuda()
   if args.verbose >= 2 then
--- opts/opts.lua
+++ opts/opts.lua
@@ -2,34 +2,38 @@
 require 'paths'
 
 local env_name = 
-  --'space_invaders'
+  'space_invaders'
   --'seaquest'
-  'breakout'
+  --'breakout'
 
 local agent_filename = 'NeuralQLearner'
 local agent_params = 
   -- reinforce
-  'lr=0.000025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3_no_bn\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
+  --'lr=0.000025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3_no_bn\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
   -- initial lr=0.00025
-  --'lr=0.00025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3_no_bn\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
+  'lr=0.00025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
 
 -- frame-skipping technique
 local actrep = 4
 
-local reinforce = string.format(
-  '/storage/atari/%s/DQN3_0_1_breakout_FULL_Y_FULL/model.t7', env_name)
+local reinforce = 
+  ''
+  --string.format(
+  --  '/storage/atari/%s/DQN3_0_1_breakout_FULL_Y_FULL/model.t7', env_name)
 local best = true
 
 local prog_freq = 25000--10000 --frequency of progress output
-local save_freq = 50000--125000 --the model is saved every save_freq steps
-local eval_freq = 50000--250000 --frequency of greedy evaluation
-local eval_steps= 100000--125000 --number of evaluation steps
+local save_freq = 250000--125000 --the model is saved every save_freq steps
+local eval_freq = 250000 --frequency of greedy evaluation
+local eval_steps= 135000--125000 --number of evaluation steps
 
 local checkpoint_agent_name = 
-  'DQN3_0_1_FULL_Y_FULL_reinforce_lr25e-6'
+  --'DQN3_0_1_FULL_Y_FULL_reinforce_lr25e-6'
   --'DQN3_0_1_FULL_Y_FULL_reinforce'
   --'DQN3_0_1_FULL_Y_FULL'
-local checkpoint_path = string.format('/storage/atari/%s/%s', env_name, checkpoint_agent_name)
+  'DQN3_0_1_FULL_Y_FULL_BN'
+local checkpoint_path = 
+  string.format('/storage/atari/%s/%s', env_name, checkpoint_agent_name)
 local X11 = false
 
 local cmd = torch.CmdLine()
@@ -45,9 +49,9 @@ cmd:option('-env_params', 'useRGB=true', 'string of environment parameters')
 cmd:option('-pool_frms', 'type=\"max\",size=2',
   'string of frame pooling parameters (e.g.: size=2,type="max")')
 cmd:option('-actrep', actrep, 
-  'how many times to repeat action 
-    (Repeat each action selected by the agent this many time. 
-    Using a value of 4 results in the agent seeing only every 4th input frame.')
+  'how many times to repeat action ' ..
+  '(Repeat each action selected by the agent this many time. ' ..
+  'Using a value of 4 results in the agent seeing only every 4th input frame.')
 cmd:option('-random_starts', 30, 'play action 0 between 1 and random_starts ' ..
   'number of times at the start of each training episode')
 
@@ -81,12 +85,10 @@ cmd:text()
 local opts = cmd:parse(arg)
 os.execute('mkdir -p '..opts.checkpoint_path)
 print('===> Saving everything to: '..opts.checkpoint_path)
---[[
 os.execute(string.format('mkdir -p %s/gifs',opts.checkpoint_path))
 os.execute(string.format('mkdir -p %s/logs',opts.checkpoint_path))
 os.execute(string.format('cp opts/opts.lua %s', opts.checkpoint_path))
 os.execute(string.format('cp *.lua %s', opts.checkpoint_path))
---]]
 io.flush()
 
 return opts
