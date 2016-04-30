6905802c2bef01188d1a7b03dd4030df4c6daa9d
taey16 <taey1600@gmail.com>
2016-04-30 17:38:18 +0900

    Comit

--- opts/opts.lua
+++ opts/opts.lua
@@ -11,19 +11,19 @@ local env_name =
 local agent_filename = 'NeuralQLearner'
 local agent_params = 
   -- explict reinforce
-  'lr=0.000025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
+  --'lr=0.000025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
   -- initial lr=0.00025
-  --'lr=0.00025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
+  'lr=0.00025,eq=1,ep_end=0.1,ep_endt=replay_memory,discount=0.99,hist_len=4,learn_start=50000,replay_memory=1000000,update_freq=4,n_replay=1,network=\"convnet_atari3\",preproc=\"net_downsample_2x_full_y\",state_dim=7056,minibatch_size=32,rescale_r=1,ncols=1,bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1'
 
 -- frame-skipping technique
 local actrep = 4
 
 local reinforce = 
   string.format(
-    --'')
+    '')
     --'/storage/atari/%s/DQN3_0_1_breakout_FULL_Y_FULL/model.t7', env_name)
     --'/storage/atari/%s/DQN3_0_1_seaquest_FULL_Y_FULL/model.t7', env_name)
-    '/storage/atari/%s/DQN3_0_1_FULL_Y/model.t7', env_name)
+    --'/storage/atari/%s/DQN3_0_1_FULL_Y/model.t7', env_name)
 local best = true
 
 local prog_freq = 50000--10000 --frequency of progress output
@@ -32,10 +32,10 @@ local eval_freq = 250000 --frequency of greedy evaluation
 local eval_steps= 135000--125000 --number of evaluation steps
 
 local checkpoint_agent_name = 
-  'DQN3_0_1_FULL_Y_reinforce_lr25e-6'
+  --'DQN3_0_1_FULL_Y_reinforce_lr25e-6'
   --'DQN3_0_1_FULL_Y_FULL_reinforce_lr25e-6'
   --'DQN3_0_1_FULL_Y_FULL_reinforce'
-  --'DQN3_0_1_FULL_Y'
+  'DQN3_0_1_FULL_Y'
 local checkpoint_path = 
   string.format('/storage/atari/%s/%s', env_name, checkpoint_agent_name)
 local X11 = false
